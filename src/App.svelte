2460cd1cb1c39cdf2c98ff3cce6cc6dadc79797f
breakerbay <breakerbay@users.noreply.github.com>
2021-07-05 14:27:44 +1200

    Create App.svelte

---
+++ src/App.svelte
@@ -0,0 +1,12 @@
+<script>
+  import Router from 'svelte-spa-router'
+  import routes from './routes.js';
+
+  import Navbar from './components/Navbar.svelte';
+</script>
+
+<Navbar />
+
+<div>
+    <Router {routes} />
+</div>
