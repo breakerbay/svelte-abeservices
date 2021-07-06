8d056cc72b38053d4d14beb7c680284cc073e515
breakerbay <breakerbay@users.noreply.github.com>
2021-07-07 11:50:44 +1200

    Added Clients, Contractors, HowItWorks

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,9 @@
 import Home from './components/Home.svelte'
 
+import Clients from './cdms/body/Clients.svelte'
+import Contractors from './cdms/body/Contractors.svelte'
+import HowItWorks from './cdms//body/HowItWorks.svelte'
+
 import AboutUs from './cdms/footer/AboutUs.svelte'
 import Acknowledgements from './cdms/footer/Acknowledgements.svelte'
 import Charges from './cdms/footer/Charges.svelte'
@@ -14,7 +18,9 @@ import NotFound from './components/NotFound.svelte'
 // Route dictionary
 export default {
     '/': Home,
-    
+    '/clients': Clients,
+    '/contractors': Contractors,
+    '/how': HowItWorks,    
     '/about': AboutUs,
     '/acknowledgements': Acknowledgements,  
     '/charges': Charges,  
