11ed32edaabff636f1d38b683dd1ffa40ff278df
breakerbay <breakerbay@users.noreply.github.com>
2021-07-06 16:01:26 +1200

    Create AboutUs.svelte

---
+++ src/cdms/footer/AboutUs.svelte
@@ -0,0 +1,31 @@
+<svelte:head>
+    <title>ABE Services - About Us</title>
+</svelte:head>
+
+<blockquote>
+    <h2>About ABE Services</h2>
+
+    <p><strong>ABE Services</strong> is  a privately owned Canberra based company formed in 2002 to develop
+        and provide  a Compliance Data Management Service (<strong>CDMS</strong>) for building,
+        construction and  related industries. ABE Services sees the CDMS
+        as the way putting  into practice the sound management principles of Quality
+        Assurance (QA) without the pain and paperwork it typically seems to involve
+        in the building and construction industry.
+    </p>
+    <h3>ABE Services Pty Ltd's Directors:</h3>
+    <ul>
+        <li>John Anderson BE, MEngSc, FIEAust, CPEng</li>
+        <li>Rob Beasley BSc, Dip Comp Sc</li>
+        <li>Mike Evans MA, FQSA, FIEAust, CPEng</li>
+        <li>Richard Hancock BSc</li>
+    </ul>
+
+    <p>Mike and John have each worked in the building and construction industry for over thirty years and
+        also have expertise in the area of Quality Management, being registered Senior
+        Quality Auditors and members of the select working group that wrote the Standards
+        Australia publication &#8220;HB90.3 &#8211; The Construction Industry Guide
+        to ISO 9001:2000&#8221;.<br>
+        Rob and Richard are software architects and programmers with particular experience
+        in major databases and IT security.</p>
+    <p>Contact us via <a href="mailto:abeservices@outlook.com">abeservices@outlook.com</a> if you would like more information about <strong>ABE Services</strong>  or the  <strong>CDMS</strong>. </p>
+</blockquote>
