8099323604aca53b458b65a73cd9d10928edd223
breakerbay <breakerbay@users.noreply.github.com>
2021-07-06 17:47:15 +1200

    Add files via upload

---
+++ src/cdms/footer/Acknowledgements.svelte
@@ -0,0 +1,23 @@
+<svelte:head>
+    <title>ABE Services - Acknowledgements</title>
+</svelte:head>
+
+<blockquote>
+    <h2>Acknowledgements</h2>
+
+    <p>ABE Services gratefully acknowledges the following:</p>
+
+    <ul>
+        <li>
+            The assistance of the <em><strong>ACT Territory Government </strong></em>in supporting the
+            commercialisation undertaken as part of the development of the CDMS;
+        </li>
+        <li>
+            The encouragement and support of the <em><strong>Master Builders Association of the ACT</strong></em>;
+        </li>
+        <li>
+            The <em><strong>Contractors</strong></em> in the ACT who have assisted by participation in the trials of the
+            CDMS.
+        </li>
+    </ul>
+</blockquote>
