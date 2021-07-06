00c30b68d3e920b2e60acce97d864c70229ffb69
breakerbay <breakerbay@users.noreply.github.com>
2021-07-06 18:30:00 +1200

    Update Charges.svelte

--- src/cdms/footer/Charges.svelte
+++ src/cdms/footer/Charges.svelte
@@ -1,3 +1,62 @@
 <svelte:head>
     <title>ABE Services - Charges</title>
 </svelte:head>
+
+<blockquote>
+    <h2>Charges</h2>
+    <p align="center">(Last
+        Updated 14/10/04)</p>
+    <table width="684" height="100" border="1" align="center">
+        <tr>
+            <td><p align="center">The
+                rates below are the normal charges.&nbsp; In recognition of
+                the tremendous support from the Master Builders Association of the
+                ACT (MBA of ACT) a special rate providing a very significant saving
+                is currently available for members of the MBA of ACT.&nbsp; MBA of
+                ACT members can contact ABE Services for further details at <a href="mailto:abeservices@outlook.com">abeservices@outlook.com</a></p></td>
+        </tr>
+    </table>
+    <p>The charge (inclusive of GST) to a business entity for the CDMS ITP Service is on a monthly basis
+        as follows:</p>
+
+    <table width="768" height="96" border="0">
+        <tr>
+            <td width="380">One
+                registered Checker in month</td>
+            <td width="378">$200</td>
+        </tr>
+        <tr>
+            <td valign="top">Between two and five registered Checkers in month</td>
+            <td>$200 for first Checker plus $150 for each subsequent Checker</td>
+        </tr>
+        <tr>
+            <td>Between six and ten registered Checkers in month&nbsp;</td>
+            <td>$800 for the first five Checkers then $125 a Checker</td>
+        </tr>
+        <tr>
+            <td>More than ten registered Checkers in month</td>
+            <td>$1,425 for the first ten Checkers, then $100 a Checker.</td>
+        </tr>
+    </table>
+
+    <p>A registered Checker is
+        someone registered to use the CDMS on a personal digital assistant (PDA) such
+        as a Palm Pilot. There is no charge for employees of the business entity who
+        are not registered Checkers using the CDMS provided that, other than Guest
+        Checkers, they are not involved in recording compliance information. There
+        is no charge for Guest Checkers provided use by Guest Checkers, is only minor/occasional.</p>
+    <p>Registered Checkers who
+        are not an employee (permanent, casual or contract) of the business entity
+        will be treated for charging purposes as belonging to a separate business
+        entity.</p>
+    <p>For the purposes of the
+        above schedule, a month is a calendar month commencing with the first day
+        of the month and ending with the last day of the month.</p>
+    <p>A 15% reduction applies
+        to the above rates when the service is purchased and paid for 12 months in
+        advance, i.e. for Advance Purchase Accounts. Advance Purchase Accounts may
+        be varied to increase the number of Checkers. In such cases payment for the
+        variation for the remaining period is to be paid in advance and the reduced
+        rate will apply. A reduction of the number of Checkers, either as originally
+        subscribed or as varied, does not entitle the purchaser to any refund.</p>
+</blockquote>
