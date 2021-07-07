f4fb805ee064f7cab0701f7a8dcd545fbcd7380c
breakerbay <breakerbay@users.noreply.github.com>
2021-07-07 16:40:26 +1200

    Update News.svelte

--- src/cdms/navbar/News.svelte
+++ src/cdms/navbar/News.svelte
@@ -1,3 +1,26 @@
 <svelte:head>
     <title>ABE Services - News</title>
 </svelte:head>
+
+<blockquote>
+    <div align="center">(Last updated 1/5/05)</div>
+    <h2>CDMS Starts Commercial Operations</h2>
+    <p>Following trials in 2004, the first customer, Urban Contractors Pty Ltd, signed up for the CDMS in December.</p>
+    <p>The CDMS is now being used on several projects in the ACT, including four by Urban Contractors Pty Ltd.</p>
+    <h2>CDMS Features in Canberra &quot;Innovation and Beyond&quot; Forum </h2>
+    <p>The CDMS featured as one of the case study presentations at the Business ACT sponsored forum &quot;Innovation
+        and Beyond&quot; held at the Museum of Australia on 10 March 2005. Download
+        a <a href="<%=request.getContextPath()%>/downloads/InnovationForum.ppt"
+             tppabs="<%=request.getContextPath()%>/downloads/InnovationForum.ppt">PDF</a> copy of the
+        InnovationForum.ppt PowerPoint presentation.</p>
+    <h2><strong>CDMS Features in ACT Engineering Excellence Awards</strong></h2>
+    <p>In September 2005 the Canberra Division of Engineers Australia conducted the
+        ACT Engineering Excellence Awards. The CDMS featured in the submission of NJ
+        Construction Pty Ltd, which achieved a Highly Commended Award. NJ Construction
+        used the CDMS to monitor the construction of the Murrarie 110kV transmission
+        line through environmentally sensitive mangrove swamps. Helicopters were used
+        to lay draw wires and then state of the art stringing machinery was used to
+        complete the stringing of the transmission line. Further details can be found
+        at <a href="http://www.ieaust.org.au/about_us/divisions/canberra/index.html ">http://www.ieaust.org.au/about_us/divisions/canberra/index.html </a>
+    </p>
+</blockquote>
