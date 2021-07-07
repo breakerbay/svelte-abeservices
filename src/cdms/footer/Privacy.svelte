347fc001cd0bf945adb47b40f34a9f70065df6be
breakerbay <breakerbay@users.noreply.github.com>
2021-07-07 18:25:24 +1200

    Update Privacy.svelte

--- src/cdms/footer/Privacy.svelte
+++ src/cdms/footer/Privacy.svelte
@@ -1,3 +1,120 @@
 <svelte:head>
     <title>ABE Services - Privacy</title>
 </svelte:head>
+
+<p align="center"><strong>ABE Services</strong></p>
+
+<h2 align="center">Privacy Statement</h2>
+
+<p align="center">(Last Updated 20/5/04)</p>
+
+<blockquote>
+    <p>ABE Services is committed to protecting your privacy. You can visit most pages on the ABE
+        Services Pty Ltd web site without giving ABE Services any information about yourself.</p>
+    <p>For you, as a customer, for the Compliance Data Management Service (CDMS) we do need some information,
+        in particular your contact details, to provide the services that you request,
+        and this privacy statement explains our data collection and use in these situations.
+        This privacy statement applies to the ABE Services website www.abeservices.com.au
+        and all its built in CDMS.</p>
+    <p>You, as the Customer,
+        or your nominee(s), may input personal information of other people into the
+        CDMS, e.g. names of your employees who are to use the CDMS or your clients&#8217;
+        representatives you authorize to have view access to your CDMS data. Such
+        access and related information is at your discretion and ABE Services Pty
+        Ltd accepts no responsibility in regard to the privacy or other implications
+        of such information and access.</p>
+    <h4>Collection of Personal Information </h4>
+    <p>ABE Services will
+        ask you (the customer) when it needs information that personally identifies
+        you (personal information) or allows ABE Services to contact you.
+        Generally, this information is requested when you are registering for CDMS
+        or ordering e-mail newsletters.</p>
+    <p>Personal information collected by ABE Services often is limited to contact details except for payment.
+    </p>
+    <p>In the main, CDMS will
+        ask personal information of you (the Customer). As a customer of the CDMS
+        you may establish additional users of the service created. ABE Services Pty
+        Ltd will not ask for personal information of these users; however those details
+        (mostly email addresses) will be stored in the CDMS databases. </p>
+    <p>When registering as a
+        customer for the CDMS you may be required to pay for a service via a third
+        party e-commerce provider and in that case, you will be required to enter
+        personal information necessary for billing, such as: name, address, telephone
+        number, and credit card number.</p>
+    <p>ABE Services does NOT keep or store any details of your credit card.</p>
+    <h4>Use of your Personal Information</h4>
+    <p>ABE Services may use your personal information for the following purposes:</p>
+    <ul>
+        <li>To ensure our site and services are relevant to your needs;</li>
+        <li>To deliver the CDMS, and perhaps newsletters;</li>
+        <li>To help ABE Services, through the CDMS, create and publish solutions most relevant to you;</li>
+        <li>To alert you to product
+            and services upgrades, special offers, updated information and other new
+            services from ABE Services, if you so request;
+        </li>
+        <li>To allow you access
+            to limited-entry areas of ABE Services web site or CDMS as appropriate.ABE
+            Services Pty Ltd may disclose your personal information if required to do
+            so by law or if in the good-faith believe that such action is necessary
+            to:
+            <ul>
+                <li>conform to the edicts of the law or comply with legal process served on ABE Services</li>
+                <li>protect and defend the rights or property of ABE Services, or</li>
+                <li>act in urgent circumstances to protect the personal safety of ABE Services employees, users
+                    of ABE Services products or services, or members of the public.
+                </li>
+            </ul>
+        </li>
+    </ul>
+    <p>Your information may be stored and processed in Australia or any other country in which ABE Services host its
+        Online Services and web sites and by accepting ABE Services Terms and Conditions and Privacy
+        Policy, you consent to any such transfer of information outside of
+        your country.</p>
+    <h4>Control of your Personal Information</h4>
+    <p>When you register, or
+        otherwise give us personal information, ABE Services will not share
+        that information with third parties without your permission, other than for
+        the limited exceptions already listed. ABE Services may send out periodic
+        e-mails informing you of technical service or security issues related to the
+        CDMS. You will not be able to choose to unsubscribe to these mailings, as
+        they are considered an essential part of the service you have chosen.</p>
+    <h4>Access to your Personal Information</h4>
+    <p>We will provide you with
+        the means to ensure that your personal information is correct and current.
+        At any time you can review and edit your personal information by contacting
+        ABE Services on abeservices@outlook.com.</p>
+    <h4>Security of your Personal Information</h4>
+    <p>ABE Services is
+        committed to protecting the security of your personal information. We use
+        a variety of security technologies and procedures to help protect your personal
+        information from unauthorized access, use or disclosure. For example, we store
+        the personal information you provide in computer servers supplied by tier
+        1 ISP, with limited access and located in controlled facilities. When ABE
+        Services Pty Ltd transmit sensitive information (such as a credit card number)
+        over the internet, we protect it through the use of encryption, such as the
+        Secure Socket Layer (SSL) protocol. Further more, all CDMS services are entirely
+        SSL enabled.</p>
+    <h4>Protection of Children's Personal Information</h4>
+    <p>ABE Service&#8217;s web
+        site and its CDMS are general audience sites and do not knowingly collect
+        any personal information from children.</p>
+    <h4>Use of Cookies</h4>
+    <p>We use cookies within the secure area of our internet sites to ensure accurate information is provided
+        to you while you conduct transactions online. A 'cookie' is a small data file
+        that contains information in relation to your visit to a website. As soon
+        as you exit the secure area of our internet sites the cookie that has been
+        created is deleted. No information is stored on our cookies from one visit
+        to the next.</p>
+    <h4>Enforcement of this Privacy Statement</h4>
+    <p>If you have questions regarding this statement, you should first contact ABE Services by
+        e-mail on <a href="mailto:abeservices@outlook.com">abeservices@outlook.com</a>.</p>
+    <h4>Changes to this Statement</h4>
+    <p>ABE Services will occasionally update this privacy statement. When ABE Services does
+        so, ABE Services will also revise the &quot;last updated&quot; date
+        at the top of the privacy statement. For material changes to this statement,
+        ABE Services will notify you by placing prominent notice on its Web
+        site.</p>
+    <h4>Contact Information</h4>
+    <p>ABE Services welcomes your comments regarding this privacy statement; please contact
+        ABE Services by e-mail on <a href="mailto:abeservices@outlook.com">abeservices@outlook.com</a>.</p>
+    <p>&nbsp;</p></blockquote>
