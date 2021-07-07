79fd4cf40aa0806d0a769ed9b8db8b0f7b9d1e7d
breakerbay <breakerbay@users.noreply.github.com>
2021-07-07 14:36:03 +1200

    Added original content

--- src/cdms/navbar/ContactUs.svelte
+++ src/cdms/navbar/ContactUs.svelte
@@ -1,3 +1,20 @@
 <svelte:head>
     <title>ABE Services - Contact Us</title>
 </svelte:head>
+
+<blockquote align="center">
+    <h2>Support / Contact Us </h2>
+    <p>ABE Services provides a free telephone and email technical support service to its CDMS customers.
+        Please note, though, that this support relates to the use of the CDMS and does not extend to consultancy advice
+        on the content of inspection and test
+        plans and the like.
+        Should you require such Quality Assurance Consultancy advice please refer to the Links screen on this web
+        site.</p>
+    <p>Technical support hours are normal
+        ACT business hours. We aim to reply to all enquiries within one working day.<br>
+        Our contact details are:</p>
+    <p>Email: <a href="mailto:abeservices@outlook.com">abeservices@outlook.com</a><br>
+        Telephone: (02) 6161 5128 <br>
+        Fax: (02) 6254 7889<br>
+        Postal Address: ABE Services, PO Box 4325, Hawker, ACT 2614, Australia</p>
+</blockquote>
