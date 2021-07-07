95c99274b4246643a6d91e4a09c4bb36abe0ddd8
breakerbay <breakerbay@users.noreply.github.com>
2021-07-07 14:50:14 +1200

    Added original content

--- src/cdms/navbar/Links.svelte
+++ src/cdms/navbar/Links.svelte
@@ -1,3 +1,43 @@
 <svelte:head>
     <title>ABE Services - Links</title>
 </svelte:head>
+
+
+<h2 align="center">Links</h2>
+
+<blockquote>
+    <table width="61%" border="0" align="center" cellpadding="1" cellspacing="1">
+        <tr>
+            <td width="58%">Australian Standards:</td>
+            <td width="42%"><a href="http://www.standards.com.au">www.standards.com.au</a></td>
+        </tr>
+        <tr>
+            <td>Master Builders Association of the ACT:</td>
+            <td><a href="http://www.mba.org.au">www.mba.org.au</a></td>
+        </tr>
+        <tr>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+        </tr>
+        <tr>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+        </tr>
+        <tr>
+            <td><strong>Quality Management consultancy services:</strong></td>
+            <td>&nbsp;</td>
+        </tr>
+        <tr>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+        </tr>
+        <tr>
+            <td>Assurance Management Services Pty Ltd:</td>
+            <td><a href="http://www.amspl.com.au">www.amspl.com.au</a></td>
+        </tr>
+        <tr>
+            <td>SAI Global:</td>
+            <td><a href="http://www.saiglobal.com.au">www.saiglobal.com.au</a></td>
+        </tr>
+    </table>
+</blockquote>
