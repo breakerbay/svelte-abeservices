ea724f20d3e54403b24c3f5366a8a4e1a4d751a7
Richard Hancock <breakerbay@hotmail.com>
2021-07-10 21:15:30 +1200

    Added content scrolling

--- src/App.svelte
+++ src/App.svelte
@@ -1,51 +1,17 @@
 <script>
-  import Router from 'svelte-spa-router'
-  import routes from './routes.js';
+    import Router from 'svelte-spa-router'
+    import routes from './routes.js';
 
-  import Navbar from './components/Navbar.svelte';
+    import Navbar from './components/Navbar.svelte';
 </script>
-
-<Navbar />
-
-<div>
-    <Router {routes} />
-</div>
-
 <div class="f-container fixed-hf">
+    <Navbar/>
     <div class="main">
-    <p>add or remove "fixed-hf" to have fixed header and footer and scrollable main section</p>
-    a<br/>
-    b<br/>
-    c<br/>
-    <div style="display:block">
-      <p>change none/block to test shorter content</p>
-    d<br/>
-    e<br/>
-    f<br/>
-    g<br/>
-    h<br/>
-    i<br/>
-    j<br/>
-    k<br/>
-    l<br/>
-    m<br/>
-    n<br/>
-    o<br/>
-    p<br/>
-    q<br/>
-    r<br/>
-    s<br/>
-    t<br/>
-    u<br/>
-    v<br/>
-    w<br/>
-    x<br/>
-    y<br/>
-    z<br/>
+        <div>
+            <Router {routes}/>
+        </div>
     </div>
-  </div>
-
-  <footer>
-    <p>I am the footer !!!</p>
-  </footer>
+    <footer>
+        <p>I am the footer !!!</p>
+    </footer>
 </div>
