42b7f5e3a51da0ea842d4140eaccdad4801f6bc4
breakerbay <breakerbay@users.noreply.github.com>
2021-07-28 17:54:02 +1200

    Only supply email address

--- src/cdms/navbar/ContactUs.svelte
+++ src/cdms/navbar/ContactUs.svelte
@@ -3,15 +3,6 @@
 </svelte:head>
 
 <blockquote align="center">
-    <h2>Support / Contact Us </h2>
-    <p>ABE Services provides a free telephone and email technical support service to its CDMS customers.
-        Please note, though, that this support relates to the use of the CDMS and does not extend to consultancy advice
-        on the content of inspection and test
-        plans and the like.
-        Should you require such Quality Assurance Consultancy advice please refer to the Links screen on this web
-        site.</p>
-    <p>Technical support hours are normal
-        ACT business hours. We aim to reply to all enquiries within one working day.<br>
-        Our contact details are:</p>
+    <h2>Contact Us</h2>
     <p>Email: <a href="mailto:abeservices@outlook.com">abeservices@outlook.com</a><br></p>
 </blockquote>
