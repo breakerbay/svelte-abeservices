26a9f1a211d57e9eb4a325249bdeb3c6821099c5
Richard Hancock <breakerbay@hotmail.com>
2021-08-01 09:03:49 +1200

    Add Mikes documents

--- src/cdms/body/Clients.svelte
+++ src/cdms/body/Clients.svelte
@@ -1,3 +1,55 @@
 <svelte:head>
-    <title>ABE Services - How It Works</title>
+    <title>ABE Services - Clients</title>
 </svelte:head>
+
+<h2> Benefits to Clients, Superintendents and Project Managers</h2>
+<hr>
+<blockquote>
+    <p><font size="2" face="Arial, Helvetica, sans-serif">The CDMS provides Clients,
+        Superintendents and Project Managers with:</font></p>
+    <table width="863" border="0">
+        <tr>
+            <td><font size="2" face="Arial, Helvetica, sans-serif">&#8226; On-line
+                access to contractor&#8217;s plans for inspection and testing (ITPs);</font></td>
+        </tr>
+        <tr>
+            <td><font size="2" face="Arial, Helvetica, sans-serif">&#8226; On-line
+                access to contractors&#8217; records of checking compliance of work,
+                including for date, time and person undertaking checks; and</font></td>
+        </tr>
+        <tr>
+            <td><font size="2" face="Arial, Helvetica, sans-serif">&#8226; On-line
+                access to contractor&#8217;s nonconformance (Follow Up Action) records.</font></td>
+        </tr>
+    </table>
+    <p><font size="2" face="Arial, Helvetica, sans-serif">With the use of these
+        capabilities, combined with the contractors&#8217; use of the CDMS with its
+        advantages for assisting in their work, the benefits to Clients, Superintendents
+        and Project Managers include:</font></p>
+    <table width="859" border="0">
+        <tr>
+            <td><font size="2" face="Arial, Helvetica, sans-serif">&#8226; Enabling <em><strong>effective
+                monitoring</strong></em> arrangements that provide genuine confidence
+                in constructed works;</font></td>
+        </tr>
+        <tr>
+            <td><font size="2" face="Arial, Helvetica, sans-serif">&#8226; <em><strong>Increased
+                efficiency</strong></em> in monitoring through on-line access to
+                planning and compliance records;</font></td>
+        </tr>
+        <tr>
+            <td><font size="2" face="Arial, Helvetica, sans-serif">&#8226; Improved
+                communication; </font></td>
+        </tr>
+        <tr>
+            <td><font size="2" face="Arial, Helvetica, sans-serif">&#8226; Facilitation
+                of early identification of potential issues and problem projects;</font></td>
+        </tr>
+        <tr>
+            <td><font size="2" face="Arial, Helvetica, sans-serif">&#8226; Ready <em><strong>demonstration
+                of accountability</strong></em>.</font></td>
+        </tr>
+    </table>
+    <p>&nbsp;</p>
+    <blockquote>&nbsp; </blockquote>
+</blockquote>
